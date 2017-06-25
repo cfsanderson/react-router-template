d98a2bafe2a86fa3eb9e3a331afaf462da65def6
cfsanderson <calebsanderson@gmail.com>
2017-06-24 22:34:38 -0400

    through step 6

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 
 render((
   <Router history={hashHistory}>
-    <Route path='/' component={App}>
-      <Route path='/repos' component={Repos}/>
-      <Route path='/about' component={About}/>
+    <Route path="/" component={App}>
+      <Route path="/repos" component={Repos}/>
+      <Route path="/repos/:username/:repoName" component={Repo}/>
+      <Route path="/about" component={About}/>
     </Route>
   </Router>
 ), document.getElementById('app'))
